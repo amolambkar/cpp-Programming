8b79c9683476ff158d24337449061a920bf5448e
Amol Ambkar <47270240+amolambkar@users.noreply.github.com>
2021-03-22 16:57:59 +0000

    Add files via upload

---
+++ prime.cpp
@@ -0,0 +1,35 @@
+/** Prgram to detect prime number
+ Author : Amol Ambkar
+ PRN : 10303320181124510011 **/
+ 
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i, n;
+    bool isPrime = true;
+    
+
+    cout << "Enter a number: ";
+    cin >> n;
+
+
+    // 0 and 1 are not prime numbers
+    if (n == 0 || n == 1) {
+        isPrime = false;
+    }
+    else {
+        for (i = 2; i <= n / 2; ++i) {
+            if (n % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+    }
+    if (isPrime)
+        cout << n << " is a prime number";
+    else
+        cout << n << " is not a prime number";
+
+    return 0;
+}
